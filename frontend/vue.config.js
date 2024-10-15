0f8964387958db9cbec30f3837b43f4fa010f8f8
Vova <weborsha@gmail.com>
2024-10-15 20:37:11 +0300

    Add missed files frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
+module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        '__VUE_OPTIONS_API__': JSON.stringify(true),
+        '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false),
+      }),
+    ],
+  },
+  transpileDependencies: true,
+  devServer: {
+    port: 8082
+  }
+});
